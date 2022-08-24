2390a9d41fb48177750eb46d5061a1b936bbb033
MayuriRasam <109909876+MayuriRasam@users.noreply.github.com>
2022-08-24 20:04:08 +0530

    Add files via upload

---
+++ CapitalToSmall.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+char CapitalToSmall(char c)
+{
+	if((c >= 'A') && (c <= 'Z'))
+	{
+		return c+32;
+	}
+}
+
+int main()
+{
+	char ch='\0', cRet='\0';
+	
+	printf("Enter character\n");
+	scanf("%c",&ch);
+	cRet=CapitalToSmall(ch);
+	
+	printf("Small letter is: %c\n",cRet);
+	return 0;
+}
