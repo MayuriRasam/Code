01e74df95aa43327714e18791fd82c39b863cb9d
MayuriRasam <109909876+MayuriRasam@users.noreply.github.com>
2022-09-11 22:31:49 +0530

    Add files via upload

---
+++ ifelse.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int age=56;
+			if(age > 18)
+			{
+				Console.WriteLine("You can drive");
+			}
+			else
+			{
+				Console.WriteLine("You cannot drive");
+			}
+        }
+    }
+}
